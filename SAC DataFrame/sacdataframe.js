dfb60a21f728c9f8a704356e52193555de265997
xPAGCOE <113696274+xPAGCOE@users.noreply.github.com>
2024-01-31 17:25:35 +0100

    Add files via upload

---
+++ SAC DataFrame/sacdataframe.js
@@ -0,0 +1,89 @@
+/*!
+	SACDataFrame.js
+*/
+
+var getScriptPromisify = (src) => {
+    return new Promise(resolve => {
+      $.getScript(src, resolve)
+    })
+  }
+
+(function () {
+    const template = document.createElement('template');
+    template.innerHTML = `
+	<style>
+    :host {
+		font-size: 14px;
+		font-family: arial;
+		font-color: #346187;
+		overflow: overlay;
+    }
+	</style>
+    <div id="main_container" hidden>
+    </div>
+    `;
+
+    class SACDataFrame extends HTMLElement{
+		
+        constructor(){
+            console.log("constructor call");
+			super();
+
+            //HTML objects
+            let shadowRoot = this.attachShadow({mode:'open'});
+            shadowRoot.appendChild(template.content.cloneNode(true));
+    
+            this._props = {};
+			
+			// private attributes
+			this.df = {};
+        }
+		
+		// loadWidget
+		async loadWidget() {
+			/* https://danfo.jsdata.org/ */
+			await getScriptPromisify('https://cdn.jsdelivr.net/npm/danfojs@1.1.2/lib/bundle.min.js');
+			
+			var main_container = this.shadowRoot.getElementById('main_container');
+		}
+		
+		// SAC custom widget events handlers
+        onCustomWidgetBeforeUpdate(changedProperties) {
+			//console.log("onCustomWidgetBeforeUpdate call");
+			this._props = { ...this._props, ...changedProperties };
+		}
+
+		onCustomWidgetAfterUpdate(changedProperties) {
+			//console.log("onCustomWidgetAfterUpdate call");
+			//console.log(changedProperties);
+		}
+		
+		connectedCallback(){
+			//console.log("connectedCallback call");
+		}
+		
+		// Methods
+		newDataFrame(data, options) {
+							
+			this.df = new dfd.DataFrame(data, options);
+			
+			return this.df;
+		}
+		
+		appendRow(values, index, inplace) {
+			
+			this.df = this.df.append(values, index, {inplace:inplace});
+			
+			return this.df;
+		}
+		
+		addColumn(column, values, inplace) {
+			
+			this.df = this.df.addColumn(column, values, {inplace:inplace});
+			
+			return this.df;
+		}
+    }
+
+    window.customElements.define('com-sap-sample-sacdataframe', SACDataFrame);
+})()
