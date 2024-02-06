f08b751174ecb77af069838ff6edbafcab30669a
xPAGCOE <113696274+xPAGCOE@users.noreply.github.com>
2024-02-06 17:35:16 +0100

    Add files via upload

--- SAC DataFrame/sacdataframe.js
+++ SAC DataFrame/sacdataframe.js
@@ -582,7 +582,10 @@ var getScriptPromisify = (src) => {
 						case "-":
 							var sub_df1 = sub_df.iloc({columns: [0]});
 							var sub_df2 = sub_df.iloc({columns: ["1:"]}).sum({axis: 1});
+							console.log(sub_df1);
+							console.log(sub_df2);
 							sub_df = sub_df1.sub(sub_df2);
+							console.log(sub_df);
 							df = this.dfd.concat({ dfList: [df, sub_df], axis: 1 });
 							df.rename({ "0": column }, { inplace: true });
 							break;
